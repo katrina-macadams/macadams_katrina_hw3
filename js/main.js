abd07fb801a697d0d84ec817fa01e4fd0f5561f9
katrina-macadams <katrinamacadams@gmail.com>
2025-04-13 17:17:49 -0400

    Added GSAP
    Added some gsap features such as scramble text, and transitions

--- js/main.js
+++ js/main.js
@@ -1,28 +1,61 @@
 const app = Vue.createApp({
   created() {
     // Fetch cards data from the API
-    fetch('http://localhost:8888/tarot-final-example/public/index.php/cards')
+    fetch('http://localhost:8888/macadams_katrina_lumenvue/public/index.php/cards')
       .then(res => res.json())
       .then(data => {
-        this.cardsData = data; // Populate cardsData with the fetched data
-        this.loadingCards = false; // Hide the loader
+        this.cardsData = data; 
+        this.loadingCards = false;
+
+        this.$nextTick(() => {
+          gsap.from(".card", {
+            y: -50, 
+            opacity: 0, 
+            duration: 1, 
+            stagger: 0.3, 
+            ease: "power3.out",
+          });
+
+          gsap.fromTo(
+            ".main-title",
+            { opacity: 0 },
+            {
+              opacity: 1,
+              duration: 2,
+              onComplete: () => {
+                gsap.to(".main-title", {
+                  duration: 2,
+                  scrambleText: {
+                    text: "Welcome to the World of Tarot",
+                    chars: "⛤₊☽⋆◯⛧☾𖤓",
+                    revealDelay: 0.8,
+                  },
+                });
+              },
+            }
+          );
+        });
       })
+      
       .catch(error => {
         console.error(error);
-        this.loadingCards = false; // Hide the loader even if there's an error
+        this.loadingCards = false;
       });
   },
   data() {
     return {
-      cardsData: [], // Array to hold the cards data
-      loadingCards: true, // Show loader while fetching data
-      activeCard: null, // Track the index of the active card
+      cardsData: [], 
+      loadingCards: true, 
+      activeCard: null, 
     };
   },
   methods: {
     toggleOverlay(index) {
-      // Toggle the active card
       this.activeCard = this.activeCard === index ? null : index;
     },
+    flipCard(index) {
+      this.cardsData[index].isFlipped = !this.cardsData[index].isFlipped;
   },
+  
+},
 }).mount('#app');
